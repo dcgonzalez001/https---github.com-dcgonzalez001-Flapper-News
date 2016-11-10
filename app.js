97dfb0da3c358ef768bfae5cc7873a5e971a8320
Dominic Gonzalez <dominic.gonzalez@everyonecounts.com>
2016-11-09 17:34:35 -0800

    Angular Services

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 var app = angular.module('flapperNews', []);
 
+// service (factory) for posts
+app.factory('posts', [function(){
+	// exposes object o (defined as posts) to any other Angular module that cares to inject it
+	var o = {
+		posts: []
+	};
+	return o;
+}]);
 app.controller('MainCtrl', [
 	'$scope',
-	function($scope){
-		/* Create a $scope function that will add an object into the posts array */		
+	// injects posts service into MainCtrl
+	'posts',
+	function($scope, posts){
+		// Binds the $scope.posts variable to the posts array in service
+		$scope.posts = posts.posts;
+		// Create a $scope function that will add an object into the posts array		
 		$scope.addPost = function(){
-			/* prevents a user from submitting a post with a blank title */
+			// prevents a user from submitting a post with a blank title
 			if(!$scope.title || $scope.title === '') { return; }
-			/* addPost function retrieves the title and link entered into the form, which is bound to the $scope variable title */
+			// addPost function retrieves the title and link entered into the form, which is bound to the $scope variable title
 			$scope.posts.push({
 				title: $scope.title,
 				link: $scope.link,
 				upvotes: 0
 			});
-			/* sets title and link to blank once it has been added to the posts array */
+			// sets title and link to blank once it has been added to the posts array
 			$scope.title = '';
 			$scope.link = '';
 		};
-		/* Defines the incrementUpvotes() function in controller */
+		// Defines the incrementUpvotes() function in controller
 		$scope.incrementUpvotes = function(post) {
 			post.upvotes += 1;
 		};
-		/*  defines a list of post titles */
+		// defines a list of post titles
 		$scope.posts = [
 			{title: 'post 1', upvotes: 5},
 			{title: 'post 2', upvotes: 2},
